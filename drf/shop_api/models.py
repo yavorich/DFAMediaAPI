6e24e099014b005f61c35685dbf8b382953637dc
Andrey Agarkov <agarkov@peterpartner.net>
2023-11-24 20:31:18 +0300

    solve opt-orders task

--- drf/shop_api/models.py
+++ drf/shop_api/models.py
@@ -122,4 +122,20 @@ class Order(models.Model):
     customer = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="orders"
     )
-    product = models.ManyToManyField(Product, related_name="orders")
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, related_name="items", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product, related_name="order_items", on_delete=models.CASCADE
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
+    quantity = models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        self.price = self.product.price
+        super().save(*args, **kwargs)
--- drf/shop_api/serializers.py
+++ drf/shop_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product, Order, User
+from .models import Category, Product, Order, User, OrderItem
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -29,9 +29,29 @@ class OrderSerializer(serializers.ModelSerializer):
         read_only_fields = ("created_at", "updated_at", "products")
 
 
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = ("product", "price", "quantity")
+        read_only_fields = ("price", )
+
+
 class OptOrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(many=True)
+    items = OrderItemSerializer(many=True)
 
     class Meta:
         model = Order
-        fields = ("product", "customer", "status", "created_at", "updated_at")
+        fields = ("items", "customer", "status", "created_at", "updated_at")
+        read_only_fields = ("created_at", "updated_at")
+
+    def create(self, validated_data):
+        items_data = validated_data.pop("items")
+        order = Order.objects.create(**validated_data)
+        for item_data in items_data:
+            product = item_data["product"]
+            quantity = item_data["quantity"]
+            OrderItem.objects.create(
+                order=order, product=product, quantity=quantity
+            )
+        return order
