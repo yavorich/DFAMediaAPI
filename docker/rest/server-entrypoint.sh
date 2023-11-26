35dddbfac2cce905aab8a9e3838c0ed74253dd40
Andrey Agarkov <agarkov@peterpartner.net>
2023-11-27 01:37:38 +0300

    add createsuperuser & loaddata to server-entrypoint

--- docker/rest/server-entrypoint.sh
+++ docker/rest/server-entrypoint.sh
@@ -15,4 +15,8 @@ done
 
 python manage.py collectstatic --noinput
 
+python manage.py createsuperuser --noinput
+
+python manage.py loaddata /app/rest/data.json
+
 python manage.py runserver 0.0.0.0:8000
